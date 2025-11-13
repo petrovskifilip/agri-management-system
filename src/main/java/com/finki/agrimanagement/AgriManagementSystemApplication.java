20294af5f514b332b3c9f783ac338fbf25d9d636
Filip Petrovski <petrovskifilip15@gmail.com>
2025-11-13 16:13:18 +0100

    Initial commit

---
+++ src/main/java/com/finki/agrimanagement/AgriManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.finki.agrimanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finki/agrimanagement/AgriManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finki.agrimanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
